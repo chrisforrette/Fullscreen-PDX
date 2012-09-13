da3bf419ba8cf683c5118a0cedcfc2cc38bf68da
Chris Forrette <chris@chrisforrette.com>
2012-09-13 15:13:34 -0700

    Skeleton

---
+++ html/js/global.js
@@ -0,0 +1,26 @@
+/**
+ * ProjectName
+ * Handles all global behaviors and properties for the site
+ *
+ * Dependencies:
+ * - jQuery
+ *
+ */
+
+(function($) {
+
+    var ProjectName = {
+
+        init: function() {
+
+        }
+
+    };
+
+    window.ProjectName = ProjectName;
+
+    $(document).ready(function() {
+        ProjectName.init();
+    });
+
+})(jQuery);
